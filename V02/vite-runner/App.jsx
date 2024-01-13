174d1676b0724e6f15b6b0289db4a8c768fb6e3a
吴习章 <wuxz14@vanke.com>
2024-01-14 07:40:13 +0800

    feat: 【V02】init, 使用jsx--创建 vite 项目,在 vite项目中将 jsx 转为 js01

--- V02/vite-runner/App.jsx
+++ V02/vite-runner/App.jsx
@@ -1,11 +1,24 @@
 import React from "./core/React.js";
-const App = React.createElement(
-  "div",
-  {
-    id: "app",
-  },
-  "app-",
-  "mini-react"
-);
+const App = <div id="app">app-mini-react</div>;
+// function App
+// function App() {
+//   return <div id="app">app-mini-react</div>;
+// }
+console.log("App", App);
+// {
+//   type: "div",
+//   props: {
+//     id: "app",
+//     children: [
+//       {
+//         type: "TEXT_ELEMENT",
+//         props: {
+//           nodeValue: "app-mini-react",
+//           children: [],
+//         },
+//       },
+//     ],
+//   },
+// };
 
 export default App;
--- V02/vite-runner/main.jsx
+++ V02/vite-runner/main.jsx
@@ -1,5 +1,7 @@
+import React from "./core/React.js";
 import ReactDom from "./core/ReactDom.js";
 import App from "./App.jsx";
 
 const root = document.getElementById("root");
 ReactDom.createRoot(root).render(App);
+// ReactDom.createRoot(root).render(<App></App>);
