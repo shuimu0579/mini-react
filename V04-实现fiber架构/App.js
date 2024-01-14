cae57432b008adef093baf9809d7b4db746fe999
吴习章 <wuxz14@vanke.com>
2024-01-14 10:28:20 +0800

    feat: V04-实现fiber架构

---
+++ V04-实现fiber架构/App.js
@@ -0,0 +1,11 @@
+import React from "./core/React.js";
+const App = React.createElement(
+  "div",
+  {
+    id: "app",
+  },
+  "app-",
+  "mini-react"
+);
+
+export default App;
---
+++ V04-实现fiber架构/vite-runner/App.jsx
@@ -0,0 +1,24 @@
+import React from "./core/React.js";
+const App = <div id="app">app-mini-react</div>;
+// function App
+// function App() {
+//   return <div id="app">app-mini-react</div>;
+// }
+console.log("App", App);
+// {
+//   type: "div",
+//   props: {
+//     id: "app",
+//     children: [
+//       {
+//         type: "TEXT_ELEMENT",
+//         props: {
+//           nodeValue: "app-mini-react",
+//           children: [],
+//         },
+//       },
+//     ],
+//   },
+// };
+
+export default App;
