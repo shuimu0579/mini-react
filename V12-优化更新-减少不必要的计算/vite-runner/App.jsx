ed5492c2ec51dd85febe45723118a40f35b4e0ee
吴习章 <wuxz14@vanke.com>
2024-01-20 11:25:29 +0800

    feat: V12-优化更新-减少不必要的计算

---
+++ V12-优化更新-减少不必要的计算/vite-runner/App.jsx
@@ -0,0 +1,29 @@
+import React from "./core/React.js";
+
+let showBar = false;
+function Counter() {
+  const bar = <div>bar</div>;
+
+  function handleShowBar() {
+    showBar = !showBar;
+    React.update();
+  }
+
+  return (
+    <div>
+      Counter
+      <button onClick={handleShowBar}>showBar</button>
+      {showBar && bar}
+    </div>
+  );
+}
+function App() {
+  return (
+    <div id="app">
+      app-mini-react
+      <Counter></Counter>
+    </div>
+  );
+}
+
+export default App;
