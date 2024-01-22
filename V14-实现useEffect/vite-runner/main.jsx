881769f0fbb323777e639ed84598982db9f173b4
吴习章 <wuxz14@vanke.com>
2024-01-23 06:08:34 +0800

    feat: 删除V14-实现useEffect

--- V14-实现useEffect/vite-runner/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from "./core/React.js";
-import ReactDom from "./core/ReactDom.js";
-import App from "./App.jsx";
-
-const root = document.getElementById("root");
-ReactDom.createRoot(root).render(<App></App>);
-// ReactDom.createRoot(root).render(App);
