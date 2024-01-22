59c565d9b499616a2ce73af17a5c2c57bc6232cf
吴习章 <wuxz14@vanke.com>
2024-01-22 21:19:09 +0800

    feat: V13-实现useState-实现组件内多个useState 的调用

--- V13-实现useState/vite-runner/core/React.js
+++ V13-实现useState/vite-runner/core/React.js
@@ -215,6 +215,8 @@ function reconcileChildren(fiber, children) {
 }
 
 function updateFunctionComponent(fiber) {
+  stateHooks = [];
+  stateHookIndex = 0;
   wipFiber = fiber;
 
   // 转换链表 设置好指针
@@ -289,15 +291,20 @@ function update() {
   };
 }
 
+let stateHooks;
+let stateHookIndex;
 function useState(initial) {
   let currentFiber = wipFiber;
-  const oldHook = currentFiber.alternate?.stateHook;
+  const oldHook = currentFiber.alternate?.stateHooks[stateHookIndex];
 
   const stateHook = {
     state: oldHook ? oldHook.state : initial,
   };
 
-  currentFiber.stateHook = stateHook;
+  stateHookIndex++;
+  stateHooks.push(stateHook);
+
+  currentFiber.stateHooks = stateHooks;
 
   function setState(action) {
     stateHook.state = action(stateHook.state);
